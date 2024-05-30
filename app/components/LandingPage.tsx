bff7408b2931e161acaad8e0833a25a48e4af787
Moksh parashar <parasharm239@gmail.com>
2024-05-30 10:12:27 +0530

    initial navigation (#6)

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -1,13 +1,20 @@
+"use client";
 import Image from "next/image";
 import img from "../../public/ignite_logo.png";
 import Footer from "./Footer";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
+
 function LandingPage() {
+  const router = useRouter();
   return (
     <div className="flex items-center justify-between flex-col h-screen">
       <nav className="w-full h-20 flex items-center justify-between px-6 border-b border-red-500">
         <Image src={img} alt="ignite" width={100} height={100} />
-        <button className="w-28 h-10 bg-red-500 text-white rounded-3xl">
+        <button
+          className="w-28 h-10 bg-red-500 text-white rounded-3xl"
+          onClick={() => router.push("/views/auth")}
+        >
           Get started
         </button>
       </nav>
@@ -20,7 +27,10 @@ function LandingPage() {
         <p className="text-xl ">
           A place to read, write and deepen your understanding.
         </p>
-        <button className="w-36 h-10 bg-red-500 text-white rounded-3xl">
+        <button
+          className="w-36 h-10 bg-red-500 text-white rounded-3xl"
+          onClick={() => router.push("/views/auth")}
+        >
           Get started
         </button>
       </section>

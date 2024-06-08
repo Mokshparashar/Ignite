0e0099ee6cdf959cad271c5de613b0c7a58ac00b
Moksh parashar <parasharm239@gmail.com>
2024-06-08 22:26:15 +0530

    google auth attached (#6)

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { MdFacebook } from "react-icons/md";
+import { loginPopup } from "../utils/login.ts";
 
 interface ChildComponentProps {
   setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,10 +11,14 @@ const Login: React.FC<ChildComponentProps> = ({ setIsLogin }) => {
     <div>
       <h1 className="text-center text-3xl mt-32">Welcome back</h1>
       <div className="w-3/4 m-auto flex items-center justify-center flex-col gap-2 mt-16">
-        <div className="border-2 rounded-3xl border-red-500 px-6 py-2 flex items-center justify-start  w-full">
+        <div
+          className="border-2 rounded-3xl border-red-500 px-6 py-2 flex items-center justify-start  w-full"
+          onClick={loginPopup}
+        >
           <FcGoogle className="text-2xl text-blue-600 mr-4" />
           <span>Login with Google</span>
         </div>
+
         <div className="border-2 rounded-3xl border-red-500 px-6 py-2 flex items-center justify-start  w-full">
           <MdFacebook className="text-2xl text-blue-600 mr-4" />
           <span>Login with Facebook</span>
---
+++ app/utils/login.ts
@@ -0,0 +1,31 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "../layout";
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export function loginPopup() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      console.log(credential);
+
+      const token = credential?.accessToken;
+      // The signed-in user info.
+      console.log("name");
+      const user = result.user;
+      console.log(user)
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
